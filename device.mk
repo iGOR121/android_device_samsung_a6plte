3db14b530acd5cc7525cf636dc4423479a19ca55
Bruno Martins <bgcngm@gmail.com>
2020-02-19 21:28:38 +0000

    exynos7870-common: Override VNDK version
    Those devices were launched with O (API level 26). This forces PRODUCT_USE_VNDK to be
    set as true and consequently BOARD_VNDK_VERSION is set to 'current', which was
    previously done in BoardConfig.mk.
    
    Change-Id: Ie262e5cdc1db0b21f6a327cd707f7a15814d0dce

--- device.mk
+++ device.mk
@@ -292,6 +292,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# Treble
+PRODUCT_USE_VNDK_OVERRIDE := true
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
