e7a691981160fa79020ab321bd9a951d6a347135
Igor121 <chernokalsky89@gmail.com>
2023-02-21 23:00:54 +0300

    a6plte: hello world * bootable recovery. * dirty

---
+++ BoardConfig.mk
@@ -0,0 +1,85 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/a6plte
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := a6plte
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineage_a6plte_defconfig
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := \
+    --board SRPRB26B003RU \
+    --kernel_offset 0x00008000 \
+    --ramdisk_offset 0x02000000 \
+    --second_offset 0x00F00000 \
+    --tags_offset 0x01E00000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_PAGESIZE :=  2048
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8953
+
+# Board
+TARGET_BOARD_PLATFORM := msm8953
+
+TARGET_BOOTLOADER_BOARD_NAME := msm8953
+TARGET_NO_BOOTLOADER := true
+
+# Bluetooth
+# BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Filesystem
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_DTBOIMG_PARTITION_SIZE := 2097152
+BOARD_VENDORIMAGE_PARTITION_SIZE := 503316480
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3313500160
+BOARD_ODMIMAGE_PARTITION_SIZE := 377487360
+BOARD_CACHEIMAGE_PARTITION_SIZE := 314572800
+BOARD_HIDDENIMAGE_PARTITION_SIZE := 5242880
+BOARD_OMRIMAGE_PARTITION_SIZE := 20971520
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 26372714496
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
+
+
+# Screen density
+TARGET_SCREEN_DENSITY := 410
+
+# Inherit from the proprietary version
+# -include vendor/samsung/a6plte/BoardConfigVendor.mk
