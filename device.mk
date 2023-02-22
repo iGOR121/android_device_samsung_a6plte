879ea84144fc25bb99446c02a6bdc2dcf3b8b5ec
Igor121 <chernokalsky89@gmail.com>
2023-02-22 13:32:46 +0300

    a6plte: include telephony

--- device.mk
+++ device.mk
@@ -274,3 +274,16 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
+
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
