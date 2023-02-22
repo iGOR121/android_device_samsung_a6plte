854b82c10089b4ef237ac3e2f17afb7c25646f46
Igor121 <chernokalsky89@gmail.com>
2023-02-22 13:33:42 +0300

    a6plte: include thermal-engine

--- device.mk
+++ device.mk
@@ -287,3 +287,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
+
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
