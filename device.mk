9b5efe5baf3990632b51da4d5c8ff7f2a793954f
Igor121 <chernokalsky89@gmail.com>
2023-02-22 12:42:56 +0300

    a6plte: import qcom configs from stock

--- device.mk
+++ device.mk
@@ -48,6 +48,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# QCOM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
 # Init files
 PRODUCT_PACKAGES += \
     init.a6plte.rc \
