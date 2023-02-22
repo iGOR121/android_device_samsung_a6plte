6282c2d32cb7082930663f727b3654fbeda97531
Igor121 <chernokalsky89@gmail.com>
2023-02-22 13:11:27 +0300

    a6plte: build HIDL shim

--- device.mk
+++ device.mk
@@ -182,6 +182,18 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0
+
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Init files
 PRODUCT_PACKAGES += \
     init.a6plte.rc \
