136b978a6aff649d7b9149ef5495c552741d094b
Igor121 <chernokalsky89@gmail.com>
2023-02-28 06:42:58 +0300

    a6plte: build wcnss_service * yeah it took me 4 days to realize

--- device.mk
+++ device.mk
@@ -381,7 +381,8 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf \
     libqmiservices_shim \
-    libcld80211.vendor
+    libcld80211.vendor \
+    wcnss_service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
